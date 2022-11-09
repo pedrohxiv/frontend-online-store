35b6c27626ff70f6e4081644d6b53642494efeba
Matheus Biaggio <matheusbiaggio@gmail.br>
2022-11-09 15:49:29 -0300

    termino req01

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,15 @@
 export async function getCategories() {
   // Implemente aqui
+  const categoriesAPI = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const fetchedCategoriesAPI = await categoriesAPI.json();
+  return fetchedCategoriesAPI;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const categoriesAndQueryAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const fetchedcategoriesAndQueryAPI = await categoriesAndQueryAPI.json();
+  return fetchedcategoriesAndQueryAPI;
 }
 
 export async function getProductById() {
