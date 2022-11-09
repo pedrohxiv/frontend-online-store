05f4b6292b68f161efe634171f900291d1bca7ad
Matheus Harmmer <mharmmer@hotmai.com>
2022-11-09 17:02:00 -0300

    Requisito 3 realizado e testado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
+      <Route exact path="/cart" component={ ShoppingCart } />
     </Switch>
   );
 }
