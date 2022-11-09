2a81f21fc67aeebd5b25b37cdce6cb8c5c623aae
Ana Dantas <anadantas232@gmail.com>
2022-11-09 17:41:53 -0300

    Req 04 complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  state = { categorys: [] };
+
+  async componentDidMount() {
+    const valores = await getCategories();
+    this.setState({ categorys: valores });
+  }
+
   render() {
+    const { categorys } = this.state;
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <div data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>
+        <div>
+          {categorys.map((category) => (
+            <button
+              key={ category.id }
+              type="button"
+              data-testid="category"
+            >
+              {category.name}
+
+            </button>
+          ))}
+        </div>
+      </div>
     );
   }
 }
