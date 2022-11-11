fb6ad5f359a08beb6df8cf6f2dfd157c41ad3364
Arthur Carvalho <arthurcrsouto@gmail.com>
2022-11-11 15:00:48 -0300

    req 7 complete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ShoppingCart from './components/ShoppingCart';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
       <Route exact path="/cart" component={ ShoppingCart } />
+      <Route path="/products-details/:id" component={ ProductDetails } />
     </Switch>
   );
 }
